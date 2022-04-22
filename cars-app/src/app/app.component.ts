329fdc92196e5ac1df5c9903164aafbc083e2385
DoroshenkoDevelop <Printcodestudio@gmail.com>
2022-04-22 13:17:44 +0300

    angular

--- cars-app/src/app/app.component.ts
+++ cars-app/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component, HostListener } from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 import {FormBuilder, Validators} from "@angular/forms";
+import {AppService} from "./app.service";
 
 @Component({
   selector: 'app-root',
@@ -8,84 +9,22 @@ import {FormBuilder, Validators} from "@angular/forms";
 })
 export class AppComponent {
 
-  carsData = [
-    {
-      image: "1.png",
-      name: "Lamborghini Huracan Spyder",
-      gear: "Полный",
-      engine: 5.2,
-      places: 2,
-    },
-    {
-      image: "2.png",
-      name: "Chevrolet Corvette",
-      gear: "Полный",
-      engine: 5.2,
-      places: 2,
-    },
-    {
-      image: "3.png",
-      name: "Ferrari California",
-      gear: "Полный",
-      engine: 5.2,
-      places: 2,
-    },
-    {
-      image: "4.png",
-      name: "Lamborghini Urus",
-      gear: "Полный",
-      engine: 5.2,
-      places: 2,
-    },
-    {
-      image: "5.png",
-      name: "Audi R8",
-      gear: "Полный",
-      engine: 5.2,
-      places: 2,
-    },
-    {
-      image: "6.png",
-      name: "Chevrolet Camaro",
-      gear: "Полный",
-      engine: 5.2,
-      places: 2,
-    },
-    {
-      image: "7.png",
-      name: "Dodge Challenger",
-      gear: "Полный",
-      engine: 5.2,
-      places: 2,
-    },
-    {
-      image: "8.png",
-      name: "Nissan GT-R ",
-      gear: "Полный",
-      engine: 5.2,
-      places: 2,
-    },
-    {
-      image: "9.png",
-      name: "Maserati Quattroporte",
-      gear: "Полный",
-      engine: 5.2,
-      places: 2,
-    },
-  ];
-
-
   priceForm = this.fb.group({ //валидация форм
     name: ['', Validators.required],
     phone: ['', Validators.required],
     car: ['', Validators.required],
   })
 
-  constructor(private fb: FormBuilder) {
+  carsData: any;
 
+  constructor(private fb: FormBuilder, private appService: AppService) {
   }
 
-  goScroll(target:HTMLElement,car?: any){
+  ngOnInit(){
+    this.appService.getData().subscribe(carsData =>this.carsData = carsData)
+  }
+
+  goScroll(target: HTMLElement, car?: any) {
     target.scrollIntoView({behavior: "smooth"});
     if (car) {
       this.priceForm.patchValue({car: car.name});
@@ -93,21 +32,34 @@ export class AppComponent {
   }
 
   trans: any;
+
   @HostListener('document:mousemove', ['$event'])
   onMouseMove(e: MouseEvent) {
     this.trans = {transform: 'translate3d(' + ((e.clientX * 0.3) / 8) + 'px,' + ((e.clientY * 0.3) / 8) + 'px,0px)'};
   }
 
   bgPos: any;
+
   @HostListener('document:scroll', ['$event'])
   onScroll() {
     this.bgPos = {backgroundPositionX: '0' + (0.3 * window.scrollY) + 'px'};
   }
 
-  onSubmit(){
-    if (this.priceForm.valid){
-      alert("Спасибо за заявку, мы свяжемся с вами в ближайшее время!");
-      this.priceForm.reset();
-    }
+  onSubmit() {
+    /* if (this.priceForm.valid){*/
+
+    this.appService.sendQuery(this.priceForm.value)
+      .subscribe(
+        {
+          next: (response: any) => {
+            alert(response.message);
+            this.priceForm.reset();
+          },
+          error: (response) => {
+            alert(response.error.message);
+          }
+        }
+      );
+
   }
 }
